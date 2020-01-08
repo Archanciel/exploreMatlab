ce2c97cbbd22825ccce75eb8d7e3b5a4a868300e
Archanciel <jp.schnyder@gmail.com>
2020-01-08 17:37:44 +0100

    set slider steps

--- matlab_interactive_gui/myslider.m
+++ matlab_interactive_gui/myslider.m
@@ -1,10 +1,10 @@
-function myslider
-x = 1:10;
+x = 1:100;
 hplot = plot(x,0*x);
-h = uicontrol('style','slider','units','pixel','position',[20 20 300 20]);
+h = uicontrol('style','slider','units','pixel','position',[20 20 300 20],'sliderstep',[1/(10-1), 2/(10-1)],'max',10,'min',1, 'value',1);
 addlistener(h,'ContinuousValueChange',@(hObject, event) makeplot(hObject, event,x,hplot));
 
 function makeplot(hObject,event,x,hplot)
-n = get(hObject,'Value');
-set(hplot,'ydata',x.^n);
-drawnow;
+    n = get(hObject,'Value')
+    set(hplot,'ydata',x.^n);
+    drawnow;
+end

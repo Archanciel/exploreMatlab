bd2e7c23b36dbb0179177d0107c0368ac9eb9688
Archanciel <jp.schnyder@gmail.com>
2020-01-24 18:31:11 +0100

    improv

--- matlab_oo/MyClassWithEvents.m
+++ matlab_oo/MyClassWithEvents.m
@@ -7,6 +7,7 @@ classdef MyClassWithEvents < handle
       function triggerEvent(obj)
          notify(obj,'StateChange')
          notify(obj,'OtherEvent')
+         obj.notify('OtherEvent') % alternate way of sending notification
       end
    end
 end
